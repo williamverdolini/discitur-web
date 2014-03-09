bbb12fe36ba60d47cda01a507eac5166a92c09e7
William Verdolini <william.verdolini@gmail.com>
2014-03-09 01:33:50 +0100

    refactoring inheriting base class

--- app/modules/lesson/LessonSideBarCtrl.js
+++ app/modules/lesson/LessonSideBarCtrl.js
@@ -1,43 +1,36 @@
 ﻿angular.module('Lesson')
     .controller('LessonSideBarCtrl', [
         '$scope',
-        'LabelService',
         'AuthService',
         '$state',
         'lessonData',
+        'DisciturBaseCtrl',
+        '$injector',
         function (
             $scope,
-            LabelService,
             AuthService,
             $state,
-            lessonData
-            ) {
-            //-------- private method -------
-            // get label from LabelService dictionary
-            var _getLabel = function (label) {
-                return LabelService.get('LessonSideBarCtrl', label);
-            }
+            lessonData,
+            DisciturBaseCtrl,
+            $injector
+             ) {
+            // inherit Discitur Base Controller
+            $injector.invoke(DisciturBaseCtrl, this, { $scope: $scope });
 
-            //-------- public method -------
-            // Invoke search service for paging through state transition to preserve paging history
-            // the state transition is forced cause the same params could be used in previous navigations
-
+            //-------- private properties -------
+            $scope._ctrl = 'LessonSideBarCtrl';
 
             //--------- public properties ------
             $scope.labels = {
-                editLessonButton: _getLabel('editLessonButton')
+                editLessonButton: $scope.getLabel('editLessonButton')
             };
-
-            console.log(AuthService.user.isLogged)
-            console.log(lessonData.author.username)
 
             $scope.local = {
                 user: AuthService.user,
                 lesson: lessonData
             }
 
-            console.log(lessonData)
-
+            //-------- public method -------
             $scope.actions = {
                 editLesson: function () {
                     console.log(lessonData)
@@ -46,47 +39,6 @@
                 }
             }
 
-            //--------- Controller initialization ------
-            // detach static bindings (labels)
-            var counter = 0;
-            var _watchers = null;
-            /*
-            var _detachStaticWatchers = $scope.$watch(function () {
-                // first digest cycle: find static watchers
-                if (_watchers == null) {
-                    _watchers = [];
-                    var _reLabels = /^{{labels\..*}}/
-                    for (var i = $scope.$$watchers.length - 1; i >= 0; i--) {
-                        if ($scope.$$watchers[i].exp &&
-                            $scope.$$watchers[i].exp.exp &&
-                            _reLabels.test($scope.$$watchers[i].exp.exp)) {
-                            _watchers.push(i);
-                        }
-                    }
-                }
-                // second digest cycle: remove static watchers
-                else {
-                    for (var i = 0; i < _watchers.length; i++) {
-                        $scope.$$watchers.splice(_watchers[i], 1);
-                    }
-                    _detachStaticWatchers();
-                }
-            })
-            */
-            
-            var detachStaticScope = $scope.$watch(function () {
-                counter += 1;
-                if (counter > 1) {
-                    for (var i = $scope.$$watchers.length - 1; i >= 0; i--) {
-                        if ($scope.$$watchers[i].exp &&
-                            $scope.$$watchers[i].exp.exp &&
-                            $scope.$$watchers[i].exp.exp.indexOf('{{labels.') == 0) {
-                            $scope.$$watchers.splice(i, 1);
-                            detachStaticScope();
-                        }
-                    }
-                }
-            })
-            
+            //--------- Controller initialization ------            
         }
     ]);
