58bc4aca49c662bb26b46e28b1dfff990b13559d
William Verdolini <william.verdolini@gmail.com>
2014-03-17 14:10:25 +0100

    correct message for title search

--- app/modules/common/LabelDictionary.js
+++ app/modules/common/LabelDictionary.js
@@ -40,7 +40,7 @@
 
         noLessonIdFound: "Oooops...la Lezione non esiste! <br>Segnalalo al <a href='mailto:support@discitur.org'>supporto tecnico</a>",
         viewMore: "Approfondisci >>",
-        keywordPlaceholder: "Ricerca la lezione per titolo, scuola, classe ed altro...",
+        keywordPlaceholder: "Ricerca la lezione per titolo",
         advKeyword: "Titolo",
         disciplinePlaceholder: "Disciplina",
         schoolPlaceholder: "Scuola",
