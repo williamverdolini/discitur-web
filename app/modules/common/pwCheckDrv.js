c46143a60003c78bae4658c05154e35aaa4d2fb2
William Verdolini <william.verdolini@gmail.com>
2014-03-22 17:35:02 +0100

    Confirm Password validation Directive

---
+++ app/modules/common/pwCheckDrv.js
@@ -0,0 +1,36 @@
+﻿angular.module('Common')
+    .directive('pwCheck', [
+        '$rootScope',
+        function ($rootScope) {
+        return {
+            restrict: 'A',
+            require: 'ngModel',            
+            
+            scope:{
+                pwCheck: '='
+            },
+            
+            link: function (scope, element, attrs, ngModel) {
+                if (!ngModel) return;
+                //var scope = scope;
+
+                var _check = function (cPwd) {
+                    var isValid = ngModel.$viewValue === cPwd;
+                    ngModel.$setValidity('pwmatch', isValid);
+                    console.log(isValid + ' - ' + cPwd + ' - ' + ngModel.$viewValue);
+                    return;
+                }
+
+                scope.$watch(
+                    function () { return scope.pwCheck },
+                    function () { _check(scope.pwCheck); }
+                    );
+                scope.$watch(
+                    function () { return ngModel.$viewValue },
+                    function () { _check(scope.pwCheck); }
+                    );
+
+            }
+        }
+        }
+    ]);
